268209a829e6024ac001f6178fe3efd88dcd686e
Zero1 <mmanickii@gmail.com>
2021-12-02 11:15:20 -0500

    Implemented reset button

--- draw.js
+++ draw.js
@@ -6,21 +6,29 @@ let canvasPanels = [];
 const canvas = document.querySelector('.canvas');
 
 let paintColor = "rgb(54, 54, 54)";
+let clearColor = "rgb(160, 160, 160)";
 
 function buildCanvas(width, height){
-    canvas.style.gridTemplateColumns = "repeat(24, 1fr)";
-    canvas.style.gridTemplateRows = "repeat(24, 1fr)";
-    for(let i = 0; i < width * height; i++){
+    gridWidth = width;
+    gridHeight = height;
+    canvas.style.gridTemplateColumns = "repeat(" + gridWidth +", 1fr)";
+    canvas.style.gridTemplateRows = "repeat(" + gridHeight +", 1fr)";
+    for(let i = 0; i < gridWidth * gridHeight; i++){
         let panel = document.createElement('div');
         panel.classList.add("canvasPanel");
         canvas.appendChild(panel);
-        panel.addEventListener("mouseover", function(){paintCel(panel)});
+        panel.addEventListener("mouseover", function(){paintCel(panel, paintColor)});
     }
 }
 
-function paintCel(myCel){
-    console.log("on cel");
-    myCel.style.backgroundColor = paintColor;
+function paintCel(myCel, color){
+    myCel.style.backgroundColor = color;
 }
 
+function clearCanvas(){
+    let cels = canvas.childNodes;
+    for (let i = 0; i < canvas.childNodes.length; i++){
+        paintCel(cels[i], clearColor);
+    }
+}
 buildCanvas(24, 24);
